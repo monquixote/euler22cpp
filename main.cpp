df55a0f36534e038df82ccec1386ace3ed09db15
Nick Long <Nick.Long@intrasonics.com>
2017-12-22 15:00:20 +0000

    A few tweaks

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <sstream>
 #include <numeric>
-#include <map>
 
 using namespace std;
 
@@ -14,18 +13,19 @@ struct Card {
 };
 
 enum hand_precidence {high, pair, two_pair, three, straight, flush, full, four, straight_flush};
-const vector<string> precidence_strings{"High", "Pair", "Two Pairs", "Three of a kind", "Straight", "Flush", "Full house", "Four of a kind", "Straight flush"};
 
 using Hand = vector<Card>;
-using HandValue = tuple<hand_precidence, int, int>;
+using HandValue = pair<hand_precidence, vector<short>>;
 
-Hand get_hand(const string &row){
-    stringstream ss(row);
+Hand get_hand(stringstream &ss){
     string s;
     Hand hand;
 
-    while (getline(ss, s, ' ')) hand.emplace_back(s[0],s[1]);
-    return move(hand);
+    for(int i=0; i<5; i++){
+        getline(ss, s, ' ');
+        hand.emplace_back(s[0],s[1]);
+    }
+    return hand;
 }
 
 bool is_flush(const Hand &h){
@@ -33,117 +33,159 @@ bool is_flush(const Hand &h){
     return  all_of(h.begin()+1, h.end(), [first](Card c){return c.suit==first;});
 }
 
-bool is_straight(const vector<short> &v){
-    short first = v.front();
-    return all_of(v.begin(), v.end(), [first](short s) mutable {return s == first++;});
+bool is_straight(const vector<short> &counts){
+    int count = 0;
+    for(auto e: counts){
+        switch(e) {
+            case 0:
+                if (count > 0) {
+                    return false;
+                }
+                break;
+            case 1:
+                ++count;
+                if(count == 5){
+                    return true;
+                }
+                break;
+            default:
+                return false;
+        }
+    }
+    return false;
 }
 
-const string cards = "23456789TJQKA";
-
 short card_val2ordinal(const char c){
+    const string cards = "23456789TJQKA";
     return (short) cards.find(c,0);
 }
 
-string ordinal2card_val(const short s){
-    return string(1, cards[s]);
-}
-
-string hand_precidence2string(const hand_precidence h){
+string hand_precidence2string(const hand_precidence &h){
+    const vector<string> precidence_strings{"High", "Pair", "Two Pairs", "Three of a kind", "Straight", "Flush", "Full house", "Four of a kind", "Straight flush"};
     return precidence_strings[h];
 }
 
-vector<short> count_vals(const vector<short> &vals){
-    vector<short> counts(static_cast<unsigned long>(*max_element(begin(vals), end(vals)) + 1), 0);
-    return accumulate(begin(vals),end(vals),counts,[](vector<short> totals,short c){ totals[c]++;return totals; });
+vector<short> count_vals(const Hand &h){
+    vector<short> counts(13, 0);
+    return accumulate(begin(h),end(h),counts,[](vector<short> totals,Card c){ totals[card_val2ordinal(c.val)]++; return totals;});
 }
 
-map<int,vector<int>> get_runs(const vector<short> &vals){
-    auto count = count_vals(vals);
-    map<int,vector<int>> runs{};
-    for (int i = 0;i<count.size();++i) {
-       if(count[i]!=0){
-           runs.try_emplace(count[i], vector<int>());
-           runs[count[i]].push_back(i);
-       }
+hand_precidence get_dupes(const vector<short> &counts){
+    auto max = *max_element(begin(counts),end(counts));
+
+    switch(max) {
+        case 1:
+            return high;
+        case 2:
+            return (count(counts.begin(), counts.end(), 2) == 2) ? two_pair : hand_precidence::pair;
+        case 3:
+            return (find(counts.begin(), counts.end(), 2) != counts.end()) ? full : three;
+        case 4:
+            return four;
+        default:
+            throw invalid_argument("No hand found");
     }
-    return runs;
 }
 
-vector<short> sort_vals(const Hand &h){
-    vector<short> vals;
-    transform(h.begin(),h.end(),back_inserter(vals),[](Card c){return card_val2ordinal(c.val);});
-    sort(vals.begin(),vals.end());
-    return vals;
+hand_precidence flush_or_straight(vector<short> &counts, const Hand &h){
+    bool flush_hand = is_flush(h);
+    bool straight_hand = is_straight(counts);
+    if (straight_hand && flush_hand) {
+        return straight_flush;
+    }
+    if (flush_hand) {
+        return hand_precidence::flush;
+    }
+    if (straight_hand) {
+        return straight;
+    }
+    return high;
 }
 
 HandValue evaluate_hand(const Hand &h){
-    bool flush_hand = is_flush(h);
-    auto sorted_vals = sort_vals(h);
-    bool straight_hand = is_straight(sorted_vals);
-    if(straight_hand && flush_hand) {
-        return HandValue{straight_flush,sorted_vals.back(),-1};
-    }
+    auto counts = count_vals(h);
+    auto prec = get_dupes(counts);
 
-    auto runs = get_runs(sorted_vals);
-    if(runs.count(4)){
-        return HandValue{four,runs[4].back(),runs[1].back()};
-    }
-    if(runs.count(3) && runs.count(2)){
-        return HandValue{full, runs[3].back(), runs[2].back()};
-    }
-    if(flush_hand){
-        return HandValue(hand_precidence::flush, sorted_vals.back(), -1);
-    }
-    if(straight_hand){
-        return HandValue(straight, sorted_vals.back(), -1);
-    }
-    if(runs.count(3)){
-        return HandValue{three, runs[3].back(), runs[1].back()};
-    }
-    if(runs.count(2) && runs[2].size() == 2){
-        return HandValue{two_pair, runs[2].back(), runs[2].front()};
-    }
-    if(runs.count(2)){
-        return HandValue{hand_precidence::pair, runs[2].back(), runs[2].back()};
-    }
-    return HandValue{high,runs[1].back(),-1};
+    return HandValue{(prec == high) ? flush_or_straight(counts,h) : prec, counts};
 }
 
+
 string handvalue2string(const HandValue &h){
-    auto [hand, major, minor] = h;
-    return hand_precidence2string(hand) + " " + ordinal2card_val(major) + (minor == -1 ? "" : " " + ordinal2card_val(minor));
+    return hand_precidence2string(h.first);
+}
+
+function<short()> make_gen(const vector<short> &counts){
+    auto max = *max_element(begin(counts),end(counts));
+    return [search = move(max), i = counts.size(), &counts]() mutable -> short  {
+        while(search>0){
+            while(i>0){
+                --i;
+                if(counts[i] == search){
+                    return i;
+                }
+            }
+            i = counts.size();
+            --search;
+        }
+        return -1;
+    };
+}
+
+bool compare_counts(const vector<short> &left, const vector<short> &right){
+    auto lfunc = make_gen(left);
+    auto rfunc = make_gen(right);
+    int l,r;
+    while(l != -1 && r != -1){
+        l = lfunc();
+        r = rfunc();
+        //cout << ordinal2card_val(l) << " " << ordinal2card_val(r) << endl;
+        if(l > r){
+            return true;
+        }
+        if(r > l){
+            return false;
+        }
+}
+    return false;
 }
 
 bool left_wins(const HandValue &left, const HandValue &right){
-    auto [lhand, lmajor, lminor] = left;
-    auto [rhand, rmajor, rminor] = right;
-    if(lhand != rhand){
-        return lhand > rhand;
+    if(left.first != right.first){
+        return left.first > right.first;
     }
-    if (lmajor != rmajor){
-        return lmajor > rmajor;
-    }
-    return lminor > rminor;
+    return compare_counts(left.second, right.second);
 }
 
 bool compare_hands(const Hand &left, const Hand &right){
     auto left_score = evaluate_hand(left);
     auto right_score = evaluate_hand(right);
-    cout << handvalue2string(left_score) << " vs " << handvalue2string(right_score) << endl;
+    //cout << handvalue2string(left_score) << " vs " << handvalue2string(right_score) << endl;
+
     return left_wins(left_score, right_score);
 }
 
 bool process_row(const string &row){
-    cout << row << endl;
-    auto left = get_hand(row.substr(0,row.length()/2));
-    auto right = get_hand(row.substr(row.length()/2+1,row.length()-1));
+    //cout << row << endl;
+    stringstream ss(row);
+    auto left = get_hand(ss);
+    auto right = get_hand(ss);
     return compare_hands(left, right);
 }
 
 int main() {
+    clock_t c;
+    c = clock();
     ifstream t("p054_poker.txt");
     string s;
-    while (getline(t,s))
-        cout << (process_row(s) ? "Left" : "Right" ) << " is the winner" << endl;
+    int i = 0;
+    while (getline(t,s)) {
+        if(process_row(s)){
+            i++;
+        }
+        //cout << (process_row(s) ? "Left" : "Right" ) << " is the winner" << endl;
+    }
+    cout << "Left won " << i << endl;
+    c = clock() - c;
+    printf ("It took me %d clicks (%f seconds).\n",c,((float)c)/CLOCKS_PER_SEC);
     return 0;
 }
